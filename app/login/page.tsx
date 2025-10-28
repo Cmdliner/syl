8971e1e93e673d042b44faad821ee7b765c91787
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2025-10-28 21:43:09 +0100

    Update: use next image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,17 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
 import { LoginForm } from "@/components/login-form"
+import Image from "next/image"
 
 export default function LoginPage() {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh lg:grid-cols-2 ">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="#" className="flex items-center gap-2 font-bold text-green">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+          <a href="#" className="flex items-center gap-2 font-bold text-6xl text-green-900">
+            {/* <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
               <GalleryVerticalEnd className="size-4" />
-            </div>
+            </div> */}
             Syl
           </a>
         </div>
@@ -21,9 +22,11 @@ export default function LoginPage() {
         </div>
       </div>
       <div className="bg-muted relative hidden lg:block">
-        <img
+        <Image
           src="/assets/bernd-dittrich-p8dvOcIjkfY-unsplash.jpg"
           alt="Image"
+          width={600}
+          height={600}
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
